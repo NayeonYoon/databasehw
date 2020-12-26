030b8f60424edd27bf5b9872959b79195e0866bc
NayeonYoon <yny1221@naver.com>
2020-12-26 14:05:22 +0900

    Add Get Customers REST API Modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import {withStyles} from "@material-ui/core/styles";
-import Paper from '@material-ui/core/Paper';
+import {withStyles} from '@material-ui/core/styles';
 
 const styles = theme => ({
   root: {
@@ -15,57 +15,53 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 3,
     overflowX: "auto"
   },
-  table: {
+  table : {
     minWidth: 1080
   }
-})
-
-
-const customers = [{
-  'id':1,
-  'name' : '윤나연',
-  'birthday' : '961222',
-  'gender' : '여자',
-  'job' : '대학생'
-},
-{
-  'id':2,
-  'name' : '홍길동',
-  'birthday' : '960305',
-  'gender' : '남자',
-  'job' : '프로그래머'
-},
-{
-  'id':3,
-  'name' : '이순신',
-  'birthday' : '921205',
-  'gender' : '남자',
-  'job' : '대학생'
-}
-]
+});
 
 class App extends Component {
-  render(){
-    const{classes} = this.props;
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow> 
 
-        </TableHead>
-        <TableBody>
-      { customers.map(c => { return (<Customer key={c.id} id={c.id} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})}
-       </TableBody>
-      </Table>
+  state={
+    customers:""
+  }
+
+  componentDidMount(){
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  render(){
+    const {classes} = this.props;
+    return (
+      <Paper className= {classes.root}>
+      <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers ? this.state.customers.map(c => { 
+              return (<Customer key={c.id} id={c.id} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+            }) : ""}
+          </TableBody>
+        </Table>
     </Paper>
   );
 }
 }
+
+
 export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -6,8 +6,31 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});       
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id':1,
+            'name' : '윤나연',
+            'birthday' : '961222',
+            'gender' : '여자',
+            'job' : '대학생'
+          },
+          {
+            'id':2,
+            'name' : '홍길동',
+            'birthday' : '960305',
+            'gender' : '남자',
+            'job' : '프로그래머'
+          },
+          {
+            'id':3,
+            'name' : '이순신',
+            'birthday' : '921205',
+            'gender' : '남자',
+            'job' : '대학생'
+          }
+    ]);
 });
+
 
 app.listen(port, () => console.log('Listening on port ${port}'));
